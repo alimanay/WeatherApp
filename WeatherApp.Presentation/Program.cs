ac0493a044a42eb9a5dcbe8d3a3f147989a6275a
Ali manay <150849054+alimanay@users.noreply.github.com>
2025-10-19 00:20:35 +0300

    Fix Dockerfile paths for deploymen

--- WeatherApp.Presentation/Program.cs
+++ WeatherApp.Presentation/Program.cs
@@ -6,36 +6,10 @@ using WeatherApp.Data.Context;
 using WeatherApp.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-string connectionString;
-// Railway'in otomatik olarak saðladýðý DATABASE_URL deðiþkenini oku.
-var dbUrl = builder.Configuration["DATABASE_URL"];
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-if (!string.IsNullOrEmpty(dbUrl))
-{
-    // Eðer dbUrl varsa, bu Railway ortamýdýr. Adresi ayrýþtýrýp doðru formata çevir.
-    var uri = new Uri(dbUrl);
-    var userInfo = uri.UserInfo.Split(':');
-
-    var dbHost = uri.Host;
-    var dbPort = uri.Port.ToString();
-    var dbUser = userInfo[0];
-    var dbPass = userInfo[1];
-    var dbName = uri.AbsolutePath.TrimStart('/');
-
-    // Npgsql'in anlayacaðý, anahtar-deðer formatýndaki baðlantý dizesini oluþtur.
-    // SSL Mode=Require ve Trust Server Certificate=true cloud veritabanlarý için önemlidir.
-    connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPass};SSL Mode=Require;Trust Server Certificate=true;";
-}
-else
-{
-    // Eðer dbUrl yoksa, bu yerel (local) ortamdýr. appsettings.json'dan oku.
-    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-}
-
-// Oluþturulan baðlantý dizesini (connectionString) kullanarak DbContext'i yapýlandýr.
+// DbContext'i bu baðlantý dizesiyle yapýlandýr.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
         connectionString,
