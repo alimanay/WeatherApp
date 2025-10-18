044bf55add1e11eace7718d553e6cd5cba35b5b7
Ali manay <150849054+alimanay@users.noreply.github.com>
2025-10-19 00:42:23 +0300

    Fix Dockerfile paths for deploymen

--- WeatherApp.Presentation/Program.cs
+++ WeatherApp.Presentation/Program.cs
@@ -24,6 +24,23 @@ builder.Services.AddControllersWithViews();
 
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.Migrate(); // Migration'larý uygula
+    }
+    catch (Exception ex)
+    {
+        // Migration sýrasýnda bir hata olursa bunu log'layabiliriz.
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Veritabaný migration'ý sýrasýnda bir hata oluþtu.");
+        // Bu aþamada programýn durmasý genellikle istenen bir durumdur,
+        // çünkü veritabaný olmadan uygulama düzgün çalýþamaz.
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -39,6 +56,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
 
 app.MapControllerRoute(
     name: "default",
