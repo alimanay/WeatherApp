e71139d193a536abbe1cd5f247267bdf81ac7f39
Ali manay <150849054+alimanay@users.noreply.github.com>
2025-10-18 22:45:05 +0300

    Proje geliştirildi

---
+++ WeatherApp.Business/WeatherDaily.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Business
+{
+    public class WeatherDaily
+    {
+        public DateTime Date { get; set; }        // Günün tarihi
+        public double MinTemp { get; set; }       // Günün en düşük sıcaklığı
+        public double MaxTemp { get; set; }       // Günün en yüksek sıcaklığı
+        public string Condition { get; set; }     // Açıklama (örnek: "parçalı bulutlu")
+        public string Icon { get; set; }
+        public int WeatherDataId { get; set; }       // Foreign key
+        public WeatherData WeatherData { get; set; }// OpenWeatherMap ikon kodu (örnek: "03d")
+    }
+}
+
